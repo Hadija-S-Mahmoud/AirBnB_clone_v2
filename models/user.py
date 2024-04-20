d6398fc2ca291ef1cbdad0dcaf654ffc0da7a0ba
Hadija-S-Mahmoud <hsmahmoud20@gmail.com>
2024-04-20 12:53:13 -0700

    Updates

--- models/user.py
+++ models/user.py
@@ -8,8 +8,7 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+    __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -18,8 +17,3 @@ class User(BaseModel, Base):
                               cascade='all, delete, delete-orphan')
         reviews = relationship('Review', backref='user',
                                cascade='all, delete, delete-orphan')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
